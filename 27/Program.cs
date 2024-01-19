5b782f883c9a30dd45546b83836f67bd762fe539
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 17:05:52 +0200

    Add files via upload

---
+++ 27/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceți elementele vectorului, separate prin spațiu: ");
+        int[] vector = CitesteVector();
+
+        Console.Write("Introduceți indexul dorit în vector (de la 0): ");
+        int index = int.Parse(Console.ReadLine());
+
+        int valoareDupaSortare = ValoareDupaSortare(vector, index);
+
+        Console.WriteLine($"Valoarea de pe poziția {index} după sortare este: {valoareDupaSortare}");
+
+
+        Console.ReadKey();
+    }
+
+    static int[] CitesteVector()
+    {
+        return Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    }
+
+    static int ValoareDupaSortare(int[] vector, int index)
+    {
+
+        if (index >= 0 && index < vector.Length)
+        {
+
+            int[] vectorSortat = vector.ToArray();
+            Array.Sort(vectorSortat);
+
+
+            return vectorSortat[index];
+        }
+        else
+        {
+            Console.WriteLine("Indexul introdus nu este valid.");
+            return -1; 
+        }
+    }
+}

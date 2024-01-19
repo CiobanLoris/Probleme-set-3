79258718f2b632017ac78e07ea9773449f815541
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 17:03:28 +0200

    Add files via upload

---
+++ 17/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceți numărul în baza 10: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduceți baza b (1 < b < 17): ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (b <= 1 || b >= 17)
+        {
+            Console.WriteLine("Baza introdusă nu este validă.");
+            return;
+        }
+
+        string rezultat = ConvertireBaza(n, b);
+
+        Console.WriteLine($"Numărul {n} în baza {b} este: {rezultat}");
+
+
+        Console.ReadKey();
+    }
+
+    static string ConvertireBaza(int numar, int baza)
+    {
+        if (numar == 0)
+        {
+            return "0";
+        }
+
+        string rezultat = "";
+
+        while (numar > 0)
+        {
+            int rest = numar % baza;
+            char cifra;
+
+            if (rest < 10)
+            {
+                cifra = (char)('0' + rest);
+            }
+            else
+            {
+                cifra = (char)('A' + rest - 10);
+            }
+
+            rezultat = cifra + rezultat;
+            numar /= baza;
+        }
+
+        return rezultat;
+    }
+}

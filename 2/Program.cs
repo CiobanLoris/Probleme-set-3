836aa77f0367e20a0471cbd8a420bcb8d1bef484
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:57:26 +0200

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti lungimea vectorului: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] vector = new int[n];
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Introduceti elementul {i + 1}: ");
+            vector[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("Introduceti valoarea k: ");
+        int k = Convert.ToInt32(Console.ReadLine());
+
+        int pozitie = GasestePozitie(vector, k);
+
+        if (pozitie != -1)
+        {
+            Console.WriteLine($"Prima pozitie in care apare {k} in vector este: {pozitie}");
+        }
+        else
+        {
+            Console.WriteLine($"{k} nu apare in vector. Rezultatul este -1.");
+        }
+
+        
+        Console.ReadKey();
+    }
+
+    static int GasestePozitie(int[] vector, int k)
+    {
+        for (int i = 0; i < vector.Length; i++)
+        {
+            if (vector[i] == k)
+            {
+                return i;
+            }
+        }
+
+        return -1; 
+    }
+}

c65792ce10c8e2a3ee2706892af8d3bf539b6aa3
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 17:12:29 +0200

    Add files via upload

---
+++ 31/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceți elementele vectorului, separate prin spațiu: ");
+        int[] vector = CitesteVector();
+
+        int rezultat = GasesteElementMajoritar(vector);
+
+        if (rezultat != -1)
+        {
+            Console.WriteLine($"Elementul majoritar este: {rezultat}");
+        }
+        else
+        {
+            Console.WriteLine("Nu există element majoritar în vector.");
+        }
+
+
+        Console.ReadKey();
+    }
+
+    static int[] CitesteVector()
+    {
+        return Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    }
+
+    static int GasesteElementMajoritar(int[] vector)
+    {
+        int candidat = -1;
+        int contor = 0;
+
+        foreach (int element in vector)
+        {
+            if (contor == 0)
+            {
+                candidat = element;
+                contor = 1;
+            }
+            else if (candidat == element)
+            {
+                contor++;
+            }
+            else
+            {
+                contor--;
+            }
+        }
+
+
+        int aparitiiCandidat = 0;
+        foreach (int element in vector)
+        {
+            if (element == candidat)
+            {
+                aparitiiCandidat++;
+            }
+        }
+
+        if (aparitiiCandidat > vector.Length / 2)
+        {
+            return candidat;
+        }
+
+        return -1;
+    }
+}
